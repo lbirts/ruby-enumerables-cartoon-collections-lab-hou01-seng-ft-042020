a3f671e4c31cd308c1c34c3ac78abf60c7c78a71
Lauren Birts <birtslauren@gmail.com>
2020-03-25 04:39:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index(1) { |dwarves, index| puts "#{index}. #{dwarves}"
+  dwarves.each_with_index(1) { |dwarf, index| puts "#{index}. #{dwarf}"
   words_array.collect {|word| word.reverse }
   # Your code here
 end
