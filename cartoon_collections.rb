41e91e64a8ccb444eee9878dc7b67d47323f23f5
Lauren Birts <birtslauren@gmail.com>
2020-03-25 04:42:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index(1) { |dwarf, index| puts "#{index}. #{dwarf}" }
+  dwarves.each.with_index(1) { |dwarf, index| puts "#{index}. #{dwarf}" }
   # Your code here
 end
 
